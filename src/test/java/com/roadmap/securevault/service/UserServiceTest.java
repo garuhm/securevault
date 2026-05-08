085516d0dd39e99e4db55c4b31f43751df8f648e
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-05-08 16:02:01 -0400

    test

--- src/test/java/com/roadmap/securevault/service/UserServiceTest.java
+++ src/test/java/com/roadmap/securevault/service/UserServiceTest.java
@@ -20,6 +20,7 @@ import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
