883c355c770a2fb982188ecaaebee9bc9c4d642a
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-05-10 21:57:57 -0400

    oauth2 branch start

--- src/main/java/com/roadmap/securevault/exception/InvalidRefreshTokenException.java
+++ src/main/java/com/roadmap/securevault/exception/InvalidRefreshTokenException.java
@@ -3,7 +3,6 @@ package com.roadmap.securevault.exception;
 import io.jsonwebtoken.JwtException;
 
 public class InvalidRefreshTokenException extends JwtException {
-
     public InvalidRefreshTokenException(String message) {
         super(message);
     }
