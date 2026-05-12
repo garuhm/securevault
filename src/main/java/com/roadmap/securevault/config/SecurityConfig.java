ceeb664581a9810fca9a84e4dd4ab6e434448114
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-05-12 19:01:02 -0400

    security config cleanup

--- src/main/java/com/roadmap/securevault/config/SecurityConfig.java
+++ src/main/java/com/roadmap/securevault/config/SecurityConfig.java
@@ -52,6 +52,18 @@ public class SecurityConfig {
 //                                .requestMatchers("/api/v1/me").permitAll()
                                 .anyRequest().authenticated())
                 .authenticationProvider(authenticationProvider())
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            response.setContentType("application/json");
+                            response.getWriter().write("{\"error\": \"Unauthorized\"}");
+                        })
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                            response.setContentType("application/json");
+                            response.getWriter().write("{\"error\": \"Forbidden\"}");
+                        })
+                )
                 .oauth2Login(
                         oauth2 -> oauth2
                                 .authorizationEndpoint(auth -> auth
