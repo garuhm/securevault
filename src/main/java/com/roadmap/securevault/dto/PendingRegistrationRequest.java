a99b1815f1b2c0c2ea6ea8cc5162a050e0b2b09f
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-06-08 21:11:50 -0400

    no display name on pend reg req

--- src/main/java/com/roadmap/securevault/dto/PendingRegistrationRequest.java
+++ src/main/java/com/roadmap/securevault/dto/PendingRegistrationRequest.java
@@ -7,7 +7,6 @@ public record PendingRegistrationRequest(
         @NotEmpty(message = "Username cannot be blank")
         @Pattern(regexp = "^[a-zA-Z0-9_.]{4,32}$",
                 message = "Username must be between 4 and 32 characters and contain only letters, numbers, underscores, and periods")
-        String username,
-        String displayName
+        String username
 ) {
 }
