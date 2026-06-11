eaf0332fa66e05cb7912151927ebc83cefa09109
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-06-10 21:02:13 -0400

    implementing additional validation fortenant jwt

--- src/main/java/com/roadmap/securevault/tenant/filter/TenantJwtFilter.java
+++ src/main/java/com/roadmap/securevault/tenant/filter/TenantJwtFilter.java
@@ -4,7 +4,10 @@ import com.roadmap.securevault.common.config.properties.CookieProperties;
 import com.roadmap.securevault.common.filter.BaseJwtFilter;
 import com.roadmap.securevault.common.service.AccessJwtService;
 import com.roadmap.securevault.common.service.CookieService;
+import com.roadmap.securevault.tenant.entity.Tenant;
 import com.roadmap.securevault.tenant.service.TenantUserService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,4 +18,16 @@ public class TenantJwtFilter extends BaseJwtFilter {
                              CookieProperties cookieProperties) {
         super(accessJwtService, userService, cookieService, cookieProperties);
     }
+
+    @Override
+    protected boolean additionalValidation(String token,
+                                           UserDetails user,
+                                           HttpServletRequest request) {
+        Tenant tenant = (Tenant) request.getAttribute("tenant");
+        if (tenant == null) return false;
+
+        String tokenTenantId = accessJwtService.extractClaim(token, "tenantId");
+        return tenant.getId().toString().equals(tokenTenantId);
+    }
 }
+
