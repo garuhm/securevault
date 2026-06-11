da922e039c688038e23403eca1a084e3beae87ed
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-06-10 23:04:07 -0400

    update requests

---
+++ src/main/java/com/roadmap/securevault/common/dto/UserUpdateRequest.java
@@ -0,0 +1,7 @@
+package com.roadmap.securevault.common.dto;
+
+public interface UserUpdateRequest {
+    String username();
+    String email();
+    String password();
+}
---
+++ src/main/java/com/roadmap/securevault/tenant/dto/tenant_user/TenantUserUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.roadmap.securevault.tenant.dto.tenant_user;
+
+import com.roadmap.securevault.common.dto.UserUpdateRequest;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+
+public record TenantUserUpdateRequest(
+        @Pattern(regexp = "^[a-zA-Z0-9_.]{4,32}$",
+                message = "Username must be between 4 and 32 characters and contain only letters, numbers, underscores, and periods")
+        String username,
+
+        @Email(message = "Invalid email format")
+        String email,
+
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+                message = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one valid special character")
+        String password
+) implements UserUpdateRequest {}
