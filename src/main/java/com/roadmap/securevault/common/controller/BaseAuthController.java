2e725f2fe34c336abf6da9fb639225e7911a0bc5
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-06-10 23:07:42 -0400

    R extends base user repo AND jpa spec now

--- src/main/java/com/roadmap/securevault/common/controller/BaseAuthController.java
+++ src/main/java/com/roadmap/securevault/common/controller/BaseAuthController.java
@@ -5,13 +5,14 @@ import com.roadmap.securevault.common.repo.BaseUserRepository;
 import com.roadmap.securevault.common.service.BaseAuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-public abstract class BaseAuthController<U extends BaseUser & UserDetails, R extends BaseUserRepository<U>> {
+public abstract class BaseAuthController<U extends BaseUser & UserDetails, R extends BaseUserRepository<U> & JpaSpecificationExecutor<U>> {
 
     protected final BaseAuthService<U, R> authService;
 
