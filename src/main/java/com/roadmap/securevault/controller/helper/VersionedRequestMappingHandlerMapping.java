77dd6b5955ac1379483b2b97ea896f3d6274c16c
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-06-10 21:47:18 -0400

    jwt gets noapiversion

--- src/main/java/com/roadmap/securevault/controller/helper/VersionedRequestMappingHandlerMapping.java
+++ src/main/java/com/roadmap/securevault/controller/helper/VersionedRequestMappingHandlerMapping.java
@@ -1,6 +1,7 @@
 package com.roadmap.securevault.controller.helper;
 
 import com.roadmap.securevault.controller.annotation.ApiVersion;
+import com.roadmap.securevault.controller.annotation.NoApiVersion;
 import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
@@ -13,6 +14,10 @@ public class VersionedRequestMappingHandlerMapping extends RequestMappingHandler
     protected RequestMappingInfo getMappingForMethod(Method method, Class<?> handlerType) {
         RequestMappingInfo mapping = super.getMappingForMethod(method, handlerType);
         if (mapping == null) return null;
+
+        if (AnnotatedElementUtils.hasAnnotation(method, NoApiVersion.class)) {
+            return mapping;
+        }
 
         ApiVersion version = findApiVersion(method, handlerType);
         if (version == null) return mapping;
