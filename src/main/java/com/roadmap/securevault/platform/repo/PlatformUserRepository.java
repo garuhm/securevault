56992f7f49014e1e732483be5ac5523c3961d413
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-06-10 23:03:23 -0400

    platform users can also be searched up start (extend jpa spec in repo)

--- src/main/java/com/roadmap/securevault/platform/repo/PlatformUserRepository.java
+++ src/main/java/com/roadmap/securevault/platform/repo/PlatformUserRepository.java
@@ -2,6 +2,9 @@ package com.roadmap.securevault.platform.repo;
 
 import com.roadmap.securevault.common.repo.BaseUserRepository;
 import com.roadmap.securevault.platform.entity.PlatformUser;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PlatformUserRepository extends BaseUserRepository<PlatformUser> {
+public interface PlatformUserRepository extends
+        BaseUserRepository<PlatformUser>,
+        JpaSpecificationExecutor<PlatformUser> {
 }
