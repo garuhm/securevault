c102bdfe0e4b012edb2cd97f989852952e1f55a0
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-06-12 21:23:10 -0400

    disabled refresj token cleaner for now

--- src/main/java/com/roadmap/securevault/scheduling/RefreshTokenCleaner.java
+++ src/main/java/com/roadmap/securevault/scheduling/RefreshTokenCleaner.java
@@ -1,18 +1,18 @@
-package com.roadmap.securevault.scheduling;
-
-import com.roadmap.securevault.service.helper.RefreshJwtService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
-public class RefreshTokenCleaner {
-
-    private final RefreshJwtService refreshJwtService;
-
-    @Scheduled(fixedRate = 900000) // 15 minutes
-    public void cleanUpRevokedAndExpired() {
-        refreshJwtService.cleanUpRevokedAndExpired();
-    }
-}
+//package com.roadmap.securevault.scheduling;
+//
+//import com.roadmap.securevault.service.helper.RefreshJwtService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class RefreshTokenCleaner {
+//
+//    private final RefreshJwtService refreshJwtService;
+//
+//    @Scheduled(fixedRate = 900000) // 15 minutes
+//    public void cleanUpRevokedAndExpired() {
+//        refreshJwtService.cleanUpRevokedAndExpired();
+//    }
+//}
