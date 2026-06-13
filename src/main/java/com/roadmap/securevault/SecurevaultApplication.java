962b301d3e31e3a1e25fac107e4d72ec9af2f171
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-06-12 21:22:18 -0400

    keycloak properties + dependencies

--- src/main/java/com/roadmap/securevault/SecurevaultApplication.java
+++ src/main/java/com/roadmap/securevault/SecurevaultApplication.java
@@ -1,7 +1,7 @@
 package com.roadmap.securevault;
 
 import com.roadmap.securevault.config.properties.CookieProperties;
-import com.roadmap.securevault.config.properties.JwtProperties;
+import com.roadmap.securevault.config.properties.KeycloakProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
-        JwtProperties.class,
+        KeycloakProperties.class,
         CookieProperties.class
 })
 @EnableScheduling
---
+++ src/main/java/com/roadmap/securevault/config/properties/KeycloakProperties.java
@@ -0,0 +1,27 @@
+package com.roadmap.securevault.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "security.keycloak")
+public record KeycloakProperties(
+        String realm,
+        String authServerUrl,
+        String clientId,
+        String clientSecret
+) {
+    public String tokenUri() {
+        return authServerUrl + "/realms/" + realm + "/protocol/openid-connect/token";
+    }
+
+    public String logoutUri() {
+        return authServerUrl + "/realms/" + realm + "/protocol/openid-connect/logout";
+    }
+
+    public String adminUsersUri() {
+        return authServerUrl + "/admin/realms/" + realm + "/users";
+    }
+
+    public String adminUserSessionsUri(String userId) {
+        return authServerUrl + "/admin/realms/" + realm + "/users/" + userId + "/logout";
+    }
+}
