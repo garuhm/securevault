e54ec1c82b7da682a1733e3044f396467c1b314a
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-06-12 22:23:25 -0400

    refrehs token cleaner not needed, removing runner annotation

--- src/main/java/com/roadmap/securevault/SecurevaultApplication.java
+++ src/main/java/com/roadmap/securevault/SecurevaultApplication.java
@@ -5,14 +5,12 @@ import com.roadmap.securevault.config.properties.KeycloakProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
         KeycloakProperties.class,
         CookieProperties.class
 })
-@EnableScheduling
 public class SecurevaultApplication {
 
     public static void main(String[] args) {
